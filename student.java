b210ccc1e04de19c3d69f08e517848d1771da872
Anoop <anoopkuzhivayalil@gmail.com>
2022-05-10 16:04:45 +0530

    Student Details

---
+++ student.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class student{
+public static void main(String args[])
+{
+Scanner c= new Scanner(System.in);
+System.out.print("Enter Name:");
+String n1=c.next();
+
+Scanner u= new Scanner(System.in);
+System.out.print("Enter RollNo.:");
+int n2=u.nextInt();
+
+System.out.println("Name:"+n1);
+System.out.print("Roll Number:"+n2);
+
+}}
